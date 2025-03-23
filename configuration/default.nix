7b74192350da71af394e610a3df5bfb6bd906500
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-22 23:55:31 -0500

    initial setup

--- configuration/default.nix
+++ configuration/default.nix
@@ -3,6 +3,9 @@
     stateVersion = "24.11";
     username = "craole";
     homeDirectory = "/home/craole";
+    shellAliases = {
+      # rebuild = ''home-manager switch --flake ${self}'';
+    };
   };
   programs.home-manager.enable = true;
   imports = [];
