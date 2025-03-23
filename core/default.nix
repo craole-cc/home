3033f13c4154ea66d9fd075f146267e1e1e45a72
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-23 01:40:55 -0500

    Added an alias to format

--- core/default.nix
+++ core/default.nix
@@ -1,7 +1,6 @@
 {flake, ...}: {
   home = let
     inherit (flake) local;
-    # flake = "$HOME/Projects/home";
   in {
     stateVersion = "24.11";
     username = "craole";
@@ -12,6 +11,10 @@
       Flash = "pushd ${local};git status --porcelain >/dev/null && gitui;home-manager switch -b BaC --flake .;popd:q";
       Flush = "pushd ${local};git status --porcelain >/dev/null && gitui;nix-store --gc;home-manager expire-generations 1;popd:q";
       Flick = "Flush;Flake;Flash:q";
+      Fmt = "nix fmt";
+    };
+    sessionVariables = {
+      HOME_FLAKE = "${local}";
     };
   };
   programs.home-manager.enable = true;
