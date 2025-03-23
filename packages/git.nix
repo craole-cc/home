c1a43b7018abf308cd874cf2de775fcfe5f788f6
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-23 02:23:02 -0500

    Added gitui and gh

--- packages/git.nix
+++ packages/git.nix
@@ -1,10 +1,25 @@
-{
-  programs.git = {
-    enable = true;
-    userName = "craole-cc";
-    userEmail = "134658831+craole-cc@users.noreply.github.com";
-    extraConfig = {
-      init.defaultBranch = "main";
+{pkgs, ...}: {
+  programs = {
+    git = {
+      enable = true;
+      userName = "craole-cc";
+      userEmail = "134658831+craole-cc@users.noreply.github.com";
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+    };
+    gitui = {
+      enable = true;
+    };
+    gh = {
+      enable = true;
+      gitCredentialHelper.enable = true;
+      settings = {
+        aliases = {
+          "as" = "auth status";
+        };
+      };
+      extensions = [pkgs.gh-eco];
     };
   };
 }
