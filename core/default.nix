328a55b0200ee9edd0c4ff04d4eb6e8efe4a9fca
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-23 01:50:55 -0500

    It seems the config is not sticking system-wide. Checking

--- core/default.nix
+++ core/default.nix
@@ -6,11 +6,11 @@
     username = "craole";
     homeDirectory = "/home/craole";
     shellAliases = {
-      Flux = "pushd ${local};git status --porcelain >/dev/null && gitui;popd:q";
-      Flake = "pushd ${local};nix flake update;popd:q";
-      Flash = "pushd ${local};git status --porcelain >/dev/null && gitui;home-manager switch -b BaC --flake .;popd:q";
-      Flush = "pushd ${local};git status --porcelain >/dev/null && gitui;nix-store --gc;home-manager expire-generations 1;popd:q";
-      Flick = "Flush;Flake;Flash:q";
+      Flux = "pushd ${local};git status --porcelain >/dev/null && gitui;popd";
+      Flake = "pushd ${local};nix flake update;popd";
+      Flash = "pushd ${local};git status --porcelain >/dev/null && gitui;home-manager switch -b BaC --flake .;popd";
+      Flush = "pushd ${local};git status --porcelain >/dev/null && gitui;nix-store --gc;home-manager expire-generations 1;popd";
+      Flick = "Flush;Flake;Flash";
       Fmt = "nix fmt";
     };
     sessionVariables = {
