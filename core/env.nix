0caa0ba05a86ff761591e7ba7bc008c58eefc7f7
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-23 03:19:02 -0500

    Flushing

--- core/env.nix
+++ core/env.nix
@@ -6,8 +6,6 @@
   home = let
     inherit (flake) local;
     inherit (pkgs) writeShellScriptBin;
-    mkFlakeScript = name: script:
-      pkgs.writeShellScriptBin name script;
 
     #@ Define a helper to ensure we're in the flake directory
     beAtHome = ''
@@ -33,23 +31,23 @@
       exit 0
     '';
 
-    fly = mkFlakeScript "Fly" ''
+    fly = writeShellScriptBin "Fly" ''
       Flux nix flake update
     '';
 
-    flash = mkFlakeScript "Flash" ''
+    flash = writeShellScriptBin "Flash" ''
       Flux home-manager switch -b BaC --flake .
     '';
 
-    flush = mkFlakeScript "Flush" ''
+    flush = writeShellScriptBin "Flush" ''
       Flux nix-store --gc && home-manager expire-generations 1
     '';
 
-    flick = mkFlakeScript "Flick" ''
+    flick = writeShellScriptBin "Flick" ''
       Flush && Fly && Flash
     '';
 
-    fmtree = mkFlakeScript "Fmtree" ''
+    fmtree = writeShellScriptBin "Fmtree" ''
       Flux nix fmt
     '';
   in {
