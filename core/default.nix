49db2046260bfd7688726c987d26bc9507dec92b
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-23 02:17:33 -0500

    problems

--- core/default.nix
+++ core/default.nix
@@ -5,16 +5,16 @@
     stateVersion = "24.11";
     username = "craole";
     homeDirectory = "/home/craole";
-    shellAliases = {
-      Flux = "pushd ${local};git status --porcelain >/dev/null && gitui;popd";
-      Flake = "pushd ${local};nix flake update;popd";
-      Flash = "pushd ${local};git status --porcelain >/dev/null && gitui;home-manager switch -b BaC --flake .;popd";
-      Flush = "pushd ${local};git status --porcelain >/dev/null && gitui;nix-store --gc;home-manager expire-generations 1;popd";
-      Flick = "Flush;Flake;Flash";
-      Fmt = "nix fmt";
-    };
     sessionVariables = {
       HOME_FLAKE = "${local}";
+    };
+    shellAliases = {
+      Flux = "pushd $HOME_FLAKE;git status --porcelain >/dev/null && gitui;popd";
+      Flake = "pushd $HOME_FLAKE;nix flake update;popd";
+      Flash = "pushd $HOME_FLAKE;git status --porcelain >/dev/null && gitui;home-manager switch -b BaC --flake .;popd";
+      Flush = "pushd $HOME_FLAKE;git status --porcelain >/dev/null && gitui;nix-store --gc;home-manager expire-generations 1;popd";
+      Flick = "Flush;Flake;Flash";
+      Fmt = "nix fmt";
     };
   };
   programs.home-manager.enable = true;
