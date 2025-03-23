cc2c4735b8b617cb64ea06ac0eaf450d70630bb5
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-23 02:33:38 -0500

    used writeShellScriptBin for the aliases

--- core/default.nix
+++ core/default.nix
@@ -5,18 +5,18 @@
     stateVersion = "24.11";
     username = "craole";
     homeDirectory = "/home/craole";
-    sessionVariables = {
-      HOME_FLAKE = "${local}";
-    };
-    shellAliases = {
-      Flux = "pushd $HOME_FLAKE;git status --porcelain >/dev/null && gitui;popd";
-      Flake = "pushd $HOME_FLAKE;nix flake update;popd";
-      Flash = "pushd $HOME_FLAKE;git status --porcelain >/dev/null && gitui;home-manager switch -b BaC --flake .;popd";
-      Flush = "pushd $HOME_FLAKE;git status --porcelain >/dev/null && gitui;nix-store --gc;home-manager expire-generations 1;popd";
-      Flick = "Flush;Flake;Flash";
-      Fmt = "nix fmt";
-    };
+    # sessionVariables = {
+    #   HOME_FLAKE = "${local}";
+    # };
+    # shellAliases = {
+    #   Flux = "pushd $HOME_FLAKE;git status --porcelain >/dev/null && gitui;popd";
+    #   Flake = "pushd $HOME_FLAKE;nix flake update;popd";
+    #   Flash = "pushd $HOME_FLAKE;git status --porcelain >/dev/null && gitui;home-manager switch -b BaC --flake .;popd";
+    #   Flush = "pushd $HOME_FLAKE;git status --porcelain >/dev/null && gitui;nix-store --gc;home-manager expire-generations 1;popd";
+    #   Flick = "Flush;Flake;Flash";
+    #   Fmt = "nix fmt";
+    # };
   };
   programs.home-manager.enable = true;
-  imports = [];
+  imports = [./env.nix];
 }
---
+++ core/env.nix
@@ -0,0 +1,66 @@
+{
+  flake,
+  pkgs,
+  ...
+}: {
+  home = let
+    inherit (flake) local;
+    flakeScripts = {
+      flux = pkgs.writeShellScriptBin "Flux" ''
+        if [[ "$PWD" != "$HOME_FLAKE" ]]; then
+          pushd "$HOME_FLAKE" || exit 1
+          trap 'popd' EXIT
+        fi
+        git status --porcelain >/dev/null && gitui
+      '';
+
+      flake = pkgs.writeShellScriptBin "Flake" ''
+        if [[ "$PWD" != "$HOME_FLAKE" ]]; then
+          pushd "$HOME_FLAKE" || exit 1
+          trap 'popd' EXIT
+        fi
+        nix flake update
+      '';
+
+      flash = pkgs.writeShellScriptBin "Flash" ''
+        if [[ "$PWD" != "$HOME_FLAKE" ]]; then
+          pushd "$HOME_FLAKE" || exit 1
+          trap 'popd' EXIT
+        fi
+        git status --porcelain >/dev/null && gitui
+        home-manager switch -b BaC --flake .
+      '';
+
+      flush = pkgs.writeShellScriptBin "Flush" ''
+        if [[ "$PWD" != "$HOME_FLAKE" ]]; then
+          pushd "$HOME_FLAKE" || exit 1
+          trap 'popd' EXIT
+        fi
+        git status --porcelain >/dev/null && gitui
+        nix-store --gc
+        home-manager expire-generations 1
+      '';
+
+      flick = pkgs.writeShellScriptBin "Flick" ''
+        Flush && Flake && Flash
+      '';
+
+      fmtree = pkgs.writeShellScriptBin "Fmtree" ''
+        nix fmt
+      '';
+    };
+  in {
+    sessionVariables = {HOME_FLAKE = "${local}";};
+    packages = with flakeScripts; [
+      flux
+      flake
+      flash
+      flush
+      flick
+      fmtree
+    ];
+    shellAliases = {
+      Fmt = "fmtree";
+    };
+  };
+}
