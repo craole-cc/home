1308a86ad395fdbc32070eb9f28632ef4d3841d5
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-23 00:02:51 -0500

    adding nixfmt-tree

--- flake.nix
+++ flake.nix
@@ -13,13 +13,16 @@
     nixpkgs,
     home-manager,
     ...
-  }: {
+  }: let
+    system = "x86_64-linux";
+  in {
     homeConfigurations."craole" = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
       modules = [
         ./configuration
         ./packages
       ];
     };
+    formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
   };
 }
